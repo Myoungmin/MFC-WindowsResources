832a3080bdde421bf368e3d73c4a631b5fd614a2
Myoungmin <miung222@gmail.com>
2022-09-05 21:20:40 +0900

    Add project files.

---
+++ MFC-WindowsResources.h
@@ -0,0 +1,32 @@
+﻿
+// MFC-WindowsResources.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCWindowsResourcesApp:
+// See MFC-WindowsResources.cpp for the implementation of this class
+//
+
+class CMFCWindowsResourcesApp : public CWinApp
+{
+public:
+	CMFCWindowsResourcesApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCWindowsResourcesApp theApp;
---
+++ MFC-WindowsResourcesDlg.h
@@ -0,0 +1,33 @@
+﻿
+// MFC-WindowsResourcesDlg.h : header file
+//
+
+#pragma once
+
+
+// CMFCWindowsResourcesDlg dialog
+class CMFCWindowsResourcesDlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCWindowsResourcesDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCWINDOWSRESOURCES_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
