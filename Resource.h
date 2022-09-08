2dc2f15d424e56618567fc503581b33749389ca9
Myoungmin <miung222@gmail.com>
2022-09-08 21:33:49 +0900

    리소스에서 메뉴를 생성하고 다이얼로그에 추가하기

--- Resource.h
+++ Resource.h
@@ -6,13 +6,19 @@
 #define IDR_MAINFRAME                   128
 #define IDI_ICON1                       129
 #define IDR_MYICON                      129
+#define IDR_MENU1                       130
+#define IDM_MAINMENU                    130
+#define ID_FILE_NEW32771                32771
+#define ID_FILE_OPEN32772               32772
+#define ID_FILE_SAVE32773               32773
+#define ID_FILE_SAVEAS                  32774
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32775
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
